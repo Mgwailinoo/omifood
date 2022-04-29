adbe384d99b635f0ea32581aaebf899c9889369c
wailinoo <wloandedslpxxx@gmail.com>
2022-04-30 01:06:42 +0630

    omifood project

---
+++ resource/js/script.js
@@ -0,0 +1,115 @@
+$(document).ready(function () {
+	//Sticky nav
+	$(".js--section-features").waypoint(
+		function (direction) {
+			if (direction == "down") {
+				$("nav").addClass("sticky");
+			} else {
+				$("nav").removeClass("sticky");
+			}
+		},
+		{
+			offset: "150",
+		}
+	);
+});
+
+//Scroll Button Effect
+$(".js-btn-full").click(function () {
+	$("html,body").animate(
+		{ scrollTop: $(".js--section-plan").offset().top },
+		1000
+	);
+});
+
+$(".js-btn-ghost").click(function () {
+	$("html,body").animate(
+		{ scrollTop: $(".js--section-features").offset().top },
+		1000
+	);
+});
+
+//Scroll Button Effect
+
+$('a[href*="#"]')
+	// Remove links that don't actually link to anything
+	.not('[href="#"]')
+	.not('[href="#0"]')
+	.click(function (event) {
+		// On-page links
+		if (
+			location.pathname.replace(/^\//, "") ==
+				this.pathname.replace(/^\//, "") &&
+			location.hostname == this.hostname
+		) {
+			// Figure out element to scroll to
+			var target = $(this.hash);
+			target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+			// Does a scroll target exist?
+			if (target.length) {
+				// Only prevent default if animation is actually gonna happen
+				event.preventDefault();
+				$("html, body").animate(
+					{
+						scrollTop: target.offset().top,
+					},
+					1000,
+					function () {
+						// Callback after animation
+						// Must change focus!
+						var $target = $(target);
+						$target.focus();
+						if ($target.is(":focus")) {
+							// Checking if the target was focused
+							return false;
+						} else {
+							$target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+							$target.focus(); // Set focus again
+						}
+					}
+				);
+			}
+		}
+	});
+
+//Amination scroll
+$(".js--wp--1  ").waypoint(
+	function (direction) {
+		$(".js--wp--1 ").addClass("animated fadeIn");
+	},
+	{
+		offset: "60%",
+	}
+);
+
+$(".js--wp--2 ").waypoint(
+	function (direction) {
+		$(".js--wp--2 ").addClass("animated fadeInUpBig");
+	},
+	{
+		offset: "60%",
+	}
+);
+
+$(".js--wp--3  ").waypoint(
+	function (direction) {
+		$(".js--wp--3 ").addClass("animated fadeIn");
+	},
+	{
+		offset: "60%",
+	}
+);
+
+//Mobile nav
+$(".js--mobile-nav-icon").click(function () {
+	$(".js--main-nav").slideToggle(200);
+	if ($(".js--mobile-nav-icon i").hasClass("fa-bars")) {
+		$(".js--mobile-nav-icon i").addClass("fa-times");
+		$(".js--mobile-nav-icon i").removeClass("fa-bars");
+	} else {
+		$(".js--mobile-nav-icon i").addClass("fa-bars");
+		$(".js--mobile-nav-icon i").removeClass("fa-times");
+	}
+});
+
+//<i class="fas fa-times"></i>
